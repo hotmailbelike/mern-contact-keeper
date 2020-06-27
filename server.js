d66f38111dbe1ef6995f76436ecb5837eea5256d
Abdullah Omar <fishilike.fish@gmail.com>
2020-06-27 19:26:50 +0600

    fixed dir

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 	//set static folder
 	app.use(express.static('client/build'));
 	app.get('*', (req, res) =>
-		res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendfile(path.join(__dirname, 'client', 'build', 'index.html'))
 	);
 }
 
