9b363e2ebde4184097119bdad640c6f7a97c5787
Abdullah Omar <fishilike.fish@gmail.com>
2020-06-27 19:20:44 +0600

    prepared for deployment to heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const connectDB = require('./config/db');
 
 const app = express();
@@ -9,6 +11,7 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
+//don't need this anymore
 // app.get('/', (req, res) => {
 // 	res.json({ msg: 'Hello and welcome to Contact Keeper API ' });
 // });
@@ -17,6 +20,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/contacts', require('./routes/contacts.routes'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 
